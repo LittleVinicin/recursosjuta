f4921a0150c2d5606a33e40d54a8c8fa6b4c2a0c
LittleVinicin <vinih1447@gmail.com>
2025-03-31 19:26:12 -0300

    Mapa Juta

---
+++ menu.js
@@ -0,0 +1,24 @@
+function irate(){
+    window.location.href = 'sites/educacao/index.html'
+}
+
+function irate2(){
+    window.location.href = 'sites/transporte/index.html'
+}
+
+function irate3(){
+    window.location.href = 'sites/saude/index.html'
+}
+
+function irate4(){
+    window.location.href = 'sites/cultura/index.html'
+}
+
+function irate5(){
+    window.location.href = 'sites/lazer/index.html'
+}
+
+function irate6(){
+    window.location.href = 'sites/variados/Diversos.html'
+}
+
---
+++ sites/index.js
@@ -0,0 +1,83 @@
+function mostrarmais(){
+    var pontos = document.getElementById("pontos");
+    var maistexto = document.getElementById("mais");
+    var textobtn = document.getElementById("textobtn");
+
+    if (pontos.style.display === "none"){
+        pontos.style.display = "inline";
+        textobtn.innerHTML = "Ver Mais";
+        maistexto.style.display = "none";
+    }   else {
+        pontos.style.display = "none";
+        textobtn.innerHTML = "Ver Menos";
+        maistexto.style.display = "inline";
+    }
+}
+
+function mostrarmais2(){
+    var pontos = document.getElementById("pontos2");
+    var maistexto = document.getElementById("mais2");
+    var textobtn = document.getElementById("textobtn2");
+
+    if (pontos.style.display === "none"){
+        pontos.style.display = "inline";
+        textobtn.innerHTML = "Ver Mais";
+        maistexto.style.display = "none";
+    }   else {
+        pontos.style.display = "none";
+        textobtn.innerHTML = "Ver Menos";
+        maistexto.style.display = "inline";
+    }
+}
+
+function mostrarmais3(){
+    var pontos = document.getElementById("pontos3");
+    var maistexto = document.getElementById("mais3");
+    var textobtn = document.getElementById("textobtn3");
+
+    if (pontos.style.display === "none"){
+        pontos.style.display = "inline";
+        textobtn.innerHTML = "Ver Mais";
+        maistexto.style.display = "none";
+    }   else {
+        pontos.style.display = "none";
+        textobtn.innerHTML = "Ver Menos";
+        maistexto.style.display = "inline";
+    }
+}
+
+function mostrarmais4(){
+    var pontos = document.getElementById("pontos4");
+    var maistexto = document.getElementById("mais4");
+    var textobtn = document.getElementById("textobtn4");
+
+    if (pontos.style.display === "none"){
+        pontos.style.display = "inline";
+        textobtn.innerHTML = "Ver Mais";
+        maistexto.style.display = "none";
+    }   else {
+        pontos.style.display = "none";
+        textobtn.innerHTML = "Ver Menos";
+        maistexto.style.display = "inline";
+    }
+}
+
+function mostrarmais5(){
+    var pontos = document.getElementById("pontos5");
+    var maistexto = document.getElementById("mais5");
+    var textobtn = document.getElementById("textobtn5");
+
+    if (pontos.style.display === "none"){
+        pontos.style.display = "inline";
+        textobtn.innerHTML = "Ver Mais";
+        maistexto.style.display = "none";
+    }   else {
+        pontos.style.display = "none";
+        textobtn.innerHTML = "Ver Menos";
+        maistexto.style.display = "inline";
+    }
+}
+
+function home(){
+    window.location.href = '../../index.html'
+}
